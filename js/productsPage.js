2e9b0ff54043caf7b0855e6215f57c1b3e47ea60
Jan84 <catana_ionut@yahoo.com>
2017-12-11 21:34:19 +0200

    I made ALL the changes

--- js/productsPage.js
+++ js/productsPage.js
@@ -1,21 +1,3 @@
-var products = [
-	{ id: 0, brand: "Samsung", name: "Samsung Galaxy S8", price: 3399, category: "phone" },
-	{ id: 1, brand: "Samsung", name: "Samsung Galaxy S7", price: 1999, category: "phone" },
-	{ id: 2, brand: "Samsung", name: "Samsung Galaxy A5", price: 1299, category: "phone" },
-	{ id: 3, brand: "Huawei", name: "Huawei P10 Lite", price: 949, category: "phone" },
-	{ id: 4, brand: "Huawei", name: "Huawei Y6", price: 599, category: "phone" },
-	{ id: 5, brand: "Huawei", name: "Huawei Mate 9", price: 2899, category: "phone" },
-	{ id: 6, brand: "Apple", name: "Apple iPhone 7", price: 2999, category: "phone" },
-	{ id: 7, brand: "Apple", name: "Apple iPhone SE", price: 1499, category: "phone" },
-	{ id: 8, brand: "Apple", name: "Apple iPhone 8 Plus", price: 4849, category: "phone" },
-	{ id: 9, brand: "Sony", name: "Sony XZ Premium", price: 3099, category: "phone" },
-	{ id: 10, brand: "Sony", name: "Sony Xperia X", price: 1356, category: "phone" },
-	{ id: 11, brand: "Sony", name: "Sony Xperia Z3", price: 1399, category: "phone" },
-	{ id: 12, brand: "Xiaomi", name: "Xiaomi Redmi Note 4X", price: 849, category: "phone" },
-	{ id: 13, brand: "Xiaomi", name: "Xiaomi Mi Mix", price: 2859, category: "phone" },
-	{ id: 14, brand: "Xiaomi", name: "Xiaomi Mi 5s Plus", price: 1696, category: "phone" },
-];
-
 var productBrands = document.getElementById("product-brands");
 var sectionProducts = document.getElementById("all-products");
 var productsFilter = document.getElementById("products-filter");
@@ -72,34 +54,6 @@ productsFilter.addEventListener("change", function(e) {
 		}
 	}
 });
-
-/* Render products */
-function renderProduct(product) {
-	var newArticle = document.createElement("article");
-	newArticle.setAttribute("id", "article-content-"+product.id);
-
-	var divName = document.createElement("div");
-	divName.setAttribute("id", "product-name-"+product.id);
-	divName.innerHTML = product.name;
-
-	var divPrice = document.createElement("div");
-	divPrice.setAttribute("id", "product-price-"+product.id);
-	divPrice.innerHTML = "Pret: "+product.price;
-
-	var divBrand = document.createElement("div");
-	divBrand.setAttribute("id", "product-brand-"+product.id);
-	divBrand.innerHTML = "Brand: "+product.brand;
-
-	var addToBag = document.createElement("button");
-	addToBag.setAttribute("class", "add-to-bag");
-	addToBag.innerHTML = "Adauga in cos";
-
-	newArticle.appendChild(divName);
-	newArticle.appendChild(divPrice);
-	newArticle.appendChild(divBrand);
-	newArticle.appendChild(addToBag);
-	sectionProducts.appendChild(newArticle);
-}
 
 function init() {
 	// products sort by name
--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-function getProducts() {
-	return [{
-		name: "iPhone",
-		price: 900,
-		color: "gold",
-		id: 0,
-	},
-	{
-		name: "Galaxy",
-		price: 1200,
-		color: "black",
-		id: 1,
-	}];
-}
-
 var compareContainer = document.getElementById("compare-container");
 
 /* Add to Bag Click increment */
@@ -28,26 +13,32 @@ var addToBagClickHandler = function(e) {
 }
 sectionProducts.addEventListener('click', addToBagClickHandler);
 
+function isPopular(product) {
+	return product.popular == true;
+}
+var popularProducts = products.filter(isPopular);
+
 /* Add/remove products to/from Compare Container */
 sectionProducts.addEventListener("change", function(e) {
+	var checkbox = e.target.dataset.checkboxId;
 	if(e.target.checked) {
 		if(compareContainer.classList.contains("hidden")) {
 			compareContainer.classList.remove("hidden");
 		}
 
-		var product = getProducts().find(function(product) {
-			return e.target.dataset.checkboxId == product.id;
+		var product = popularProducts.find(function(product) {
+			return checkbox == product.id;
 		});
 
 		var containerProduct = document.createElement("div");
 		containerProduct.setAttribute("data-product-id", product.id);
-		containerProduct.setAttribute("class", "compare-product");
+		containerProduct.classList.add('compare-product')
 		containerProduct.innerText = product.name;
 		compareContainer.appendChild(containerProduct);
 
 	} else {
-		var product = document.querySelector("[data-product-id='"+e.target.dataset.checkboxId+"']");
-		product.remove();
+		var productRemove = document.querySelector("[data-product-id='"+e.target.dataset.checkboxId+"']");
+		productRemove.remove();
 	}
 		
 	if(!compareContainer.hasChildNodes()) {
@@ -55,44 +46,9 @@ sectionProducts.addEventListener("change", function(e) {
 	}
 });
 
-/* Render products */
-function renderProduct(product) {
-	var newArticle = document.createElement("article");
-	newArticle.setAttribute("id", "article-content-"+product.id);
-
-	var divName = document.createElement("div");
-	divName.setAttribute("id", "product-name-"+product.id);
-	divName.innerHTML = product.name;
-
-	var divPrice = document.createElement("div");
-	divPrice.setAttribute("id", "product-price-"+product.id);
-	divPrice.innerHTML = "Pret: "+product.price;
-
-	var divColor = document.createElement("div");
-	divColor.setAttribute("id", "product-color-"+product.id);
-	divColor.innerHTML = "Culoare: "+product.color;
-
-	var checkBox = document.createElement("input");
-	checkBox.setAttribute("data-checkbox-id", product.id);
-    checkBox.type = "checkbox";
-    checkBox.title = "Compara";
-
-	var addToBag = document.createElement("button");
-	addToBag.setAttribute("class", "add-to-bag");
-	addToBag.innerHTML = "Adauga in cos";
-
-	newArticle.appendChild(divName);
-	newArticle.appendChild(divPrice);
-	newArticle.appendChild(divColor);
-	newArticle.appendChild(addToBag);
-	newArticle.appendChild(checkBox);
-	sectionProducts.appendChild(newArticle);
-}
-
 function init() {
-	var product = getProducts();
-	for (var i = 0; i < product.length; i++) {
-		renderProduct(product[i]);	
+	for (var i = 0; i < popularProducts.length; i++) {
+		renderProduct(popularProducts[i]);	
 	}
 }
 
